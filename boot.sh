f3e4a9d7b9e6e470208b0d852d36017509cd4b73
Devashish <devashish.86@gmail.com>
2017-02-15 11:03:43 -0800

    updating envs and adding init script

--- boot.sh
+++ boot.sh
@@ -21,21 +21,48 @@ readonly LOGS_DIR="$USR_DIR/logs"
 readonly TIMESTAMP="$(date +%Y_%m_%d_%H:%M:%S)"
 readonly LOG_FILE="$LOGS_DIR/${TIMESTAMP}_logs.txt"
 readonly MAX_DEFAULT_LOG_COUNT=6
+readonly SHIPPABLE_NODE_INIT_SCRIPT="$SCRIPTS_DIR/$SHIPPABLE_NODE_INIT_SCRIPT"
+readonly SHIPPABLE_ENV="$USR_DIR/node.env"
 
 source "$LIB_DIR/logger.sh"
 
 # End Global variables #################################### 
 ###########################################################
 
-main() {
-  echo "Running node boot script........."
+info() {
+  echo "Executing node boot script"
+  echo "Init script location: $SHIPPABLE_NODE_INIT_SCRIPT"
+  if [ ! -f "$SHIPPABLE_NODE_INIT_SCRIPT" ]; then
+    echo "Error!!! No init script found at $SHIPPABLE_NODE_INIT_SCRIPT"
+    exit 1
+  fi
 
-  # source the file node.env
-  # check if SHIPPABLE_NODE_INIT is set
-  #   check if SHIPPABLE_NODE_INIT_FILE value is set
-  #   execute that script from scripts/ directory
-  # run genexec boot command
-  env
+  echo "Env file location: $SHIPPABLE_ENV"
+  if [ ! -f "$SHIPPABLE_ENV" ]; then
+    echo "Error!!! No environment file found at $SHIPPABLE_ENV"
+    exit 1
+  else
+    echo "Loading shippable envs"
+    cat $SHIPPABLE_ENV
+    source $SHIPPABLE_ENV
+  fi
+}
+
+boot() {
+  echo "Executing genexec boot..."
+}
+
+main() {
+  info
+
+  if [ $SHIPPABLE_NODE_INIT == true ]; then
+    echo "Node init set to true, initializing node"
+    source $SHIPPABLE_NODE_INIT_SCRIPT
+  else
+    echo "Node init set to false, skipping node init"
+  fi
+
+  boot
 }
 
 main
