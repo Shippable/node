dbab59703779a51c2f0a4f5abcad139a5b6fe7ff
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-12-20 13:13:56 +0530

    First draft of shipctl for Windows

--- shipctl/x86_64/WindowsServer_2016/shipctl.ps1
+++ shipctl/x86_64/WindowsServer_2016/shipctl.ps1
@@ -2,17 +2,27 @@ $ErrorActionPreference = "Stop"
 
 $shipctl_command = $args[0]
 $len = $args.Length - 1
+#$copy = $args[1..$len]
+if ($len -eq 0) {
+    $len = 1
+}
 $copy = $args[1..$len]
 
 . ./utility.ps1
 
+
 function execute_shiptctl_command() {
-    if (Get-Command $shipctl_command -CommandType Function) {
+    if (Get-Command $shipctl_command -CommandType Function -ErrorAction SilentlyContinue) {
         # Splat `copy` so it's passed as individual arguments to the function
-        Invoke-Expression "$shipctl_command @copy"
+        try {
+            Invoke-Expression "$shipctl_command @copy"
+        } catch {
+            Write-Error $_
+            exit 99
+        }
     } else {
-        Write-Output "The command $shipctl_command is not supported on this image."
-        return 99
+        Write-Error "The command $shipctl_command is not supported on this image."
+        exit 99
     }
 }
 
