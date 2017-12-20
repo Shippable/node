0a358aa39523d710959f10c6083cc038bb6c8a74
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-12-20 15:39:05 +0530

    Update machine path when installing shipctl on Windows

--- shipctl/x86_64/WindowsServer_2016/install.ps1
+++ shipctl/x86_64/WindowsServer_2016/install.ps1
@@ -9,6 +9,11 @@ Write-Output "Installing shipctl"
 Copy-Item -Force $currentDir\shipctl.ps1 $destinationDir\shipctl.ps1
 Copy-Item -Force $currentDir\utility.ps1 $destinationDir\utility.ps1
 
-
 Write-Output "Installing shippable_retry"
 Copy-Item -Force $currentDir\shippable_retry.ps1 $destinationDir\shippable_retry.ps1
+
+if (!($env:PATH.Contains($destinationDir))) {
+    Write-Output "Updating machine PATH environment variable"
+    $env:PATH="$env:PATH;$destinationDir"
+    [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine);
+}
