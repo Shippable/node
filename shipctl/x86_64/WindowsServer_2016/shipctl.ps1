50b5d500d635d952508d03d1088513c7d3c03f3b
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-12-20 14:54:08 +0530

    Add shippable_retry for Windows

--- shipctl/x86_64/WindowsServer_2016/shipctl.ps1
+++ shipctl/x86_64/WindowsServer_2016/shipctl.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop"
 
 $shipctl_command = $args[0]
 $len = $args.Length - 1
-#$copy = $args[1..$len]
 if ($len -eq 0) {
     $len = 1
 }
@@ -26,4 +25,21 @@ function execute_shiptctl_command() {
     }
 }
 
-execute_shiptctl_command $shipctl_command
+function execute_command($command) {
+    &$command @args
+    exit $LASTEXITCODE
+}
+
+if ($shipctl_command -eq "jdk") {
+    Write-Error "shipctl jdk is not yet supported on this image"
+} elseif ($shipctl_command -eq "decrypt") {
+    Write-Error "shipctl decrypt is not yet supported on this image"
+} elseif ($shipctl_command -eq "service") {
+    Write-Error "shipctl service is not yet supported on this image"
+} elseif ($shipctl_command -eq "replace") {
+    Write-Error "shipctl replace is not yet supported on this image"
+} elseif ($shipctl_command -eq "retry") {
+    execute_command shippable_retry @copy
+} else {
+    execute_shiptctl_command $shipctl_command
+}
