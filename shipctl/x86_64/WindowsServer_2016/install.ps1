5b6297c0ced08bb84157d0e08410f3dc7066a2b1
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-01-02 14:39:35 +0530

    Update install.ps1 to copy shippable_replace

--- shipctl/x86_64/WindowsServer_2016/install.ps1
+++ shipctl/x86_64/WindowsServer_2016/install.ps1
@@ -12,6 +12,9 @@ Copy-Item -Force $currentDir\utility.ps1 $destinationDir\utility.ps1
 Write-Output "Installing shippable_retry"
 Copy-Item -Force $currentDir\shippable_retry.ps1 $destinationDir\shippable_retry.ps1
 
+Write-Output "Installing shippable_replace"
+Copy-Item -Force $currentDir\shippable_replace.ps1 $destinationDir\shippable_replace.ps1
+
 if (!($env:PATH.Contains($destinationDir))) {
     Write-Output "Updating machine PATH environment variable"
     $env:PATH="$env:PATH;$destinationDir"
