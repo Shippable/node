6bce317e8d93aac7b447e9f25782f425a99fc762
Riturajcse <riturajcse1994@gmail.com>
2018-02-08 14:13:49 +0530

    creates docker folder if not present

--- initScripts/x86_64/Ubuntu_16.04/Docker_17.06.sh
+++ initScripts/x86_64/Ubuntu_16.04/Docker_17.06.sh
@@ -144,7 +144,7 @@ docker_install() {
 check_docker_opts() {
   # SHIPPABLE docker options required for every node
   echo "Adding docker options"
-
+  mkdir -p /etc/docker
   echo '{"graph": "/data", "storage-driver": "aufs"}' > /etc/docker/daemon.json
   docker_restart=true
 }
