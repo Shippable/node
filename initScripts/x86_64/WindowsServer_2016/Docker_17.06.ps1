8696b8787ad6caf90e602a15add67fea16b25e04
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-02-15 18:37:23 +0530

    separate boot and fetch logic

--- initScripts/x86_64/WindowsServer_2016/Docker_17.06.ps1
+++ initScripts/x86_64/WindowsServer_2016/Docker_17.06.ps1
@@ -4,43 +4,6 @@ $NODE_JS_VERSION = "4.8.5"
 $DOCKER_VERSION = "17.06.2-ee-5"
 $DOCKER_CONFIG_FILE="C:\ProgramData\Docker\config\daemon.json"
 
-$SHIPPABLE_RUNTIME_DIR = "$env:USERPROFILE\Shippable\Runtime"
-$BASE_UUID = New-Guid
-$BASE_DIR = "$SHIPPABLE_RUNTIME_DIR\$BASE_UUID"
-$CONTAINER_RUNTIME_DIR = "$env:USERPROFILE\Shippable\Runtime"
-$CONTAINER_BASE_DIR = "$CONTAINER_RUNTIME_DIR\$BASE_UUID"
-
-$REQPROC_DIR = "$BASE_DIR\reqProc"
-$CONTAINER_REQPROC_DIR = "$CONTAINER_BASE_DIR\reqProc"
-
-$REQEXEC_DIR = "$BASE_DIR\reqExec"
-$CONTAINER_REQEXEC_DIR = "$CONTAINER_BASE_DIR\reqExec"
-$REQEXEC_BIN_DIR = "$BASE_DIR\reqExec"
-$REQEXEC_BIN_PATH = "$REQEXEC_BIN_DIR\$NODE_ARCHITECTURE\$NODE_OPERATING_SYSTEM\dist\main\main.exe"
-
-$REQKICK_DIR = "$BASE_DIR\reqKick"
-$CONTAINER_REQKICK_DIR = "$CONTAINER_BASE_DIR\reqKick"
-$REQKICK_SERVICE_DIR = "$REQKICK_DIR\init\$NODE_ARCHITECTURE\$NODE_OPERATING_SYSTEM"
-$REQKICK_CONFIG_DIR = "$SHIPPABLE_RUNTIME_DIR\config\reqKick"
-
-$BUILD_DIR = "$BASE_DIR\build"
-$CONTAINER_BUILD_DIR = "$CONTAINER_BASE_DIR\build"
-$STATUS_DIR = "$BUILD_DIR\status"
-$SCRIPTS_DIR = "$BUILD_DIR\scripts"
-
-$REQPROC_MOUNTS = ""
-$REQPROC_ENVS = ""
-$REQPROC_OPTS = ""
-$REQPROC_CONTAINER_NAME_PATTERN = "reqProc"
-$REQPROC_CONTAINER_NAME = "$REQPROC_CONTAINER_NAME_PATTERN-$BASE_UUID"
-$REQKICK_SERVICE_NAME_PATTERN = "shippable-reqKick@"
-
-# TODO: update container directories while mounting
-$DEFAULT_TASK_CONTAINER_MOUNTS = "-v ${BUILD_DIR}:${CONTAINER_BUILD_DIR} -v ${REQEXEC_DIR}:${CONTAINER_REQEXEC_DIR}"
-$TASK_CONTAINER_COMMAND = "$CONTAINER_REQEXEC_DIR\$NODE_ARCHITECTURE\$NODE_OPERATING_SYSTEM\dist\main\main.exe"
-$DEFAULT_TASK_CONTAINER_OPTIONS = "-d --rm"
-$DOCKER_CLIENT_LATEST = "C:\Program Files\Docker\docker.exe"
-
 Function create_shippable_dir() {
   if (!(Test-Path $SHIPPABLE_RUNTIME_DIR)) {
     mkdir -p "$SHIPPABLE_RUNTIME_DIR"
@@ -139,102 +102,13 @@ Function check_docker_opts() {
   $global:DOCKER_NAT_IP=(Get-NetIPConfiguration | Where-Object InterfaceAlias -eq "vEthernet (HNS Internal NIC)").IPv4Address.IPAddress
 }
 
-Function remove_reqKick() {
-  Write-Output "Remove existing reqKick"
-
-  # remove pm2 managed reqkick services
-  if (Get-Command "pm2" -ErrorAction SilentlyContinue)
-  {
-    pm2 delete all /shippable-reqKick*/
-  }
-
-  # remove nssm managed reqkick services
-  Get-Service shippable-reqkick-* | %{
-    nssm stop $_.Name
-    nssm remove $_.Name confirm
-  }
-}
-
-Function remove_reqProc() {
-  Write-Output "Remove existing reqProc containers"
-  docker ps -a --filter "NAME=$REQPROC_CONTAINER_NAME_PATTERN" --format '{{.Names}}' | %{ docker rm -f $_ }
-}
-
-Function setup_mounts() {
-  if (Test-Path $SHIPPABLE_RUNTIME_DIR) {
-    Write-Output "Deleting Shippable runtime directory"
-    Remove-Item -recur -force $SHIPPABLE_RUNTIME_DIR
-  }
-
-  if (!(Test-Path $BASE_DIR)) {
-    mkdir -p $BASE_DIR
-  }
-
-  if (!(Test-Path $REQPROC_DIR)) {
-    mkdir -p $REQPROC_DIR
-  }
-
-  if (!(Test-Path $REQEXEC_DIR)) {
-    mkdir -p $REQEXEC_DIR
-  }
-
-  if (!(Test-Path $REQKICK_DIR)) {
-    mkdir -p $REQKICK_DIR
-  }
-
-  if (!(Test-Path $BUILD_DIR)) {
-    mkdir -p $BUILD_DIR
-  }
-
-  $global:REQPROC_MOUNTS= " -v ${BASE_DIR}:${CONTAINER_BASE_DIR} "
-}
-
-Function setup_envs() {
-  $global:REQPROC_ENVS = " -e SHIPPABLE_AMQP_URL=$SHIPPABLE_AMQP_URL " + `
-    "-e SHIPPABLE_AMQP_DEFAULT_EXCHANGE=$SHIPPABLE_AMQP_DEFAULT_EXCHANGE " + `
-    "-e SHIPPABLE_API_URL=$SHIPPABLE_API_URL " + `
-    "-e LISTEN_QUEUE='$LISTEN_QUEUE' " + `
-    "-e NODE_ID=$NODE_ID " + `
-    "-e RUN_MODE=$RUN_MODE " + `
-    "-e SUBSCRIPTION_ID=$SUBSCRIPTION_ID " + `
-    "-e NODE_TYPE_CODE=$NODE_TYPE_CODE " + `
-    "-e BASE_DIR='$CONTAINER_BASE_DIR' " + `
-    "-e REQPROC_DIR='$CONTAINER_REQPROC_DIR' " + `
-    "-e REQEXEC_DIR='$CONTAINER_REQEXEC_DIR' " + `
-    "-e REQKICK_DIR='$CONTAINER_REQKICK_DIR' " + `
-    "-e BUILD_DIR='$CONTAINER_BUILD_DIR' " + `
-    "-e REQPROC_CONTAINER_NAME='$REQPROC_CONTAINER_NAME' " + `
-    "-e DEFAULT_TASK_CONTAINER_OPTIONS='$DEFAULT_TASK_CONTAINER_OPTIONS' " + `
-    "-e EXEC_IMAGE=$EXEC_IMAGE " + `
-    "-e TASK_CONTAINER_COMMAND='$TASK_CONTAINER_COMMAND' " + `
-    "-e DEFAULT_TASK_CONTAINER_MOUNTS='$DEFAULT_TASK_CONTAINER_MOUNTS' " + `
-    "-e DOCKER_CLIENT_LATEST='$DOCKER_CLIENT_LATEST' " + `
-    "-e SHIPPABLE_DOCKER_VERSION='$DOCKER_VERSION' " + `
-    "-e IS_DOCKER_LEGACY=false " + `
-    "-e SHIPPABLE_NODE_ARCHITECTURE=$NODE_ARCHITECTURE " + `
-    "-e SHIPPABLE_NODE_OPERATING_SYSTEM=$NODE_OPERATING_SYSTEM " + `
-    "-e SHIPPABLE_RELEASE_VERSION=$SHIPPABLE_RELEASE_VERSION " + `
-    "-e DOCKER_HOST=${DOCKER_NAT_IP}:2375 " + `
-    "-e SHIPPABLE_NODE_SCRIPTS_LOCATION=$NODE_SCRIPTS_LOCATION"
-}
-
-Function setup_opts() {
-  $global:REQPROC_OPTS= " -d " + `
-    "--restart=always " + `
-    "--name=$REQPROC_CONTAINER_NAME "
-}
-
-Function boot_reqProc() {
-  Write-Output "Boot reqProc..."
+Function pull_reqProc() {
+  Write-Output "Pulling reqProc..."
   docker pull $EXEC_IMAGE
-
-  $start_cmd = "docker run $global:REQPROC_OPTS $global:REQPROC_MOUNTS $global:REQPROC_ENVS $EXEC_IMAGE"
-  Write-Output "Executing docker run command: " $start_cmd
-  iex "$start_cmd"
 }
 
-Function boot_reqKick() {
-  echo "Booting up reqKick service..."
+Function fetch_reqKick() {
+  Write-Output "Fetching reqKick..."
 
   $reqKick_tar_download_location="$env:TEMP/reqKick.tar.gz"
   Invoke-RestMethod "$REQKICK_DOWNLOAD_URL" `
@@ -244,6 +118,9 @@ Function boot_reqKick() {
   $reqKick_tar=($reqKick_tar_download_location).replace(".gz", "").replace("\", "/")
   pushd "$env:TEMP/"
   tar -xf "$reqKick_tar" --force-local
+  if (!(Test-Path $REQKICK_DIR)) {
+    mkdir -p $REQKICK_DIR
+  }
   mv reqKick-master/* $REQKICK_DIR
   Remove-Item -recur -force reqKick-master
   Remove-Item -recur -force $reqKick_tar_download_location -ErrorAction SilentlyContinue
@@ -252,38 +129,13 @@ Function boot_reqKick() {
 
   pushd $REQKICK_DIR
   npm install
-
-  $service_name = "shippable-reqkick-$BASE_UUID"
-
-  # Create stdout and stderr files for reqkick service
-  $stdout_file = "$REQKICK_DIR\$service_name.out"
-  $stderr_file = "$REQKICK_DIR\$service_name.err"
-  echo "" | Out-File -Encoding utf8 $stdout_file
-  echo "" | Out-File -Encoding utf8 $stderr_file
-
-  $nodejs_exe_path =  Get-Command "node" | Select-Object -ExpandProperty Definition
-
-  nssm install $service_name $nodejs_exe_path "$REQKICK_DIR\reqKick.app.js"
-  nssm set $service_name AppEnvironmentExtra STATUS_DIR="$STATUS_DIR" SCRIPTS_DIR="$SCRIPTS_DIR" RUN_MODE="$RUN_MODE" REQEXEC_BIN_PATH="$REQEXEC_BIN_PATH"
-  nssm set $service_name AppStdout $stdout_file
-  nssm set $service_name AppStderr $stderr_file
-  nssm start $service_name
-
   popd
 }
-
 
 create_shippable_dir
 check_win_containers_enabled
 install_prereqs
 docker_install
 check_docker_opts
-remove_reqKick
-remove_reqProc
-setup_mounts
-setup_envs
-setup_opts
-boot_reqProc
-boot_reqKick
-
-exit 0;
+pull_reqProc
+fetch_reqKick
