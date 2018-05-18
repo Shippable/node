9ecacfb007da81b1630bbf226c8a2309f96560a0
Devashish <devashish.86@gmail.com>
2018-05-18 13:17:41 +0530

    using correct variable

--- shipctl/x86_64/Ubuntu_14.04/install.sh
+++ shipctl/x86_64/Ubuntu_14.04/install.sh
@@ -21,7 +21,7 @@ readonly shippable_jdk_location="/usr/local/bin/shippable_jdk"
 if [ -f "$shippable_jdk_location" ]; then
   echo "shippable_jdk already installed on the image, skipping"
 else
-  cp $SRC_DIR/shippable_jdk /usr/local/bin/shippable_jdk
+  cp $SRC_DIR/shippable_jdk $shippable_jdk_location
 fi
 
 echo "Installing shipctl"
