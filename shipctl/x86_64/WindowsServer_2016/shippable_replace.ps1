4f760bbd0455cffaca8e0d4b6ef457cd70955362
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-05-25 15:50:13 +0530

    Update shipctl replace to support ${name} format on Windows

--- shipctl/x86_64/WindowsServer_2016/shippable_replace.ps1
+++ shipctl/x86_64/WindowsServer_2016/shippable_replace.ps1
@@ -1,14 +1,14 @@
 
 function replace() {
-    mkdir -Force $env:TEMP\shippable_replace
-
     $allEnvs = Get-ChildItem env:*
     foreach ($fileName in $args) {
         $fileContent = Get-Content "$fileName"
         foreach ($env in $allEnvs) {
-            $fileContent = $fileContent.Replace("$" + $env.Key, $env.Value)
-            $fileContent | Out-File -Encoding utf8 -FilePath "$fileName"
+            # Support ${NAME} and $NAME, in that order of preference
+            $fileContent = $fileContent.Replace("$" + "{" + $env.Key + "}", $env.Value).Replace("$" + $env.Key, $env.Value)
         }
+        # Use IO.File instead of Out-File to prevent UTF-8 BOM
+        [IO.File]::WriteAllLines(($fileName | Resolve-Path), $fileContent)
     }
 }
 
